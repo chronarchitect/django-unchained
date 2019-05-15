7bb9ec8521091d9d8d7d01815cbc364560600195
chronarchitect <aniketdas674@gmail.com>
2019-05-15 22:26:08 +0530

    Added image upload

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,5 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text', 'image')
+        exclude = ('user', )
+        fields = ('title', 'text', 'image',)
